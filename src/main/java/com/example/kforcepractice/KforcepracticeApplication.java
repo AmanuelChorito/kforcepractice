4dbc93965509101bb77b24cc81c7246dd38b22ff
AmanuelChorito <48349483+AmanuelChorito@users.noreply.github.com>
2021-10-01 16:05:31 -0500

    helo

---
+++ src/main/java/com/example/kforcepractice/KforcepracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.kforcepractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KforcepracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KforcepracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kforcepractice/KforcepracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kforcepractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KforcepracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
